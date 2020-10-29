bae9c2beceae1971e3b0d1c116b63b454a6748fe
USUARIO <USUARIO@HP>
2020-10-28 21:13:45 -0400

    agregamos la clase para buscar al inscrito

---
+++ Plataform/src/Consultas.java
@@ -0,0 +1,35 @@
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import plataform.Conexion;
+
+public class Consultas{
+    
+       public boolean existe(String nom){
+        boolean exist = false;
+        try {
+            Connection conex = Conexion.getConexion();
+            Statement sta = conex.createStatement();
+            ResultSet rs = sta.executeQuery("SELECT Nombres_y_Apellidos FROM Inscritos");
+            while (rs.next()){
+                if (nom.equals(rs.getString("Nombres_y_Apellidos"))){
+                    exist = true;
+                }
+            }
+            }catch (SQLException ex){    
+        }
+        return exist;
+    }
+    public void añadir(String nom){
+        try {
+            if(existe(nom)){
+            Connection conex = Conexion.getConexion ();
+            Statement sta = conex.createStatement();
+            ResultSet rs = sta.executeQuery("INSERT INTO Recetas VALUES ('"+ nom+")" );
+            }
+            }catch (SQLException ex){  
+        }
+    } 
+}
