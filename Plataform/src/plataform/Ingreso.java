3a25c22265aa15b40b83e837815e186d156fbeca
Nicolas Sthepan Mamani Costas <USUARIO@HP>
2020-10-29 17:41:47 -0400

    Revert "se agrego la funcionalidad para eliminar a los partisipantes que salen de la reunion"
    This reverts commit c561065cdfec09f805a6809a14ef7a47b178596a.

--- Plataform/src/plataform/Ingreso.java
+++ Plataform/src/plataform/Ingreso.java
@@ -8,7 +8,7 @@ import plataform.Conexion;
 
 public class Ingreso{
     
-       public boolean existeInscrito(String nom){
+       public boolean existe(String nom){
         boolean exist = false;
         try {
             Connection conex = Conexion.getConexion();
@@ -23,54 +23,23 @@ public class Ingreso{
         }
         return exist;
     }
-       
-       private boolean existeParticipante(String nom){
-        boolean exist = false;
-        try {
-            Connection conex = Conexion.getConexion();
-            Statement sta = conex.createStatement();
-            ResultSet rs = sta.executeQuery("SELECT nomParticipante FROM Participantes");
-            while (rs.next()){
-                if (nom.equals(rs.getString("nomParticipante"))){
-                    exist = true;
-                }
-            }
-            }catch (SQLException ex){    
-        }
-        return exist;
-    }
-       
     public void añadir(String nom){
         try {
-            if(!existeParticipante(nom)){
+            if(existe(nom)){
             Connection conex = Conexion.getConexion ();
             Statement sta = conex.createStatement();
-            int codigo = getCodigo(nom);
-            ResultSet rs = sta.executeQuery("INSERT INTO Participantes VALUES (" +codigo+",'"+ nom+"')" );
+            ResultSet rs = sta.executeQuery("INSERT INTO Registro VALUES ('"+ nom+")" );
             }
             }catch (SQLException ex){  
         }
     } 
     
-    private int getCodigo (String nom){
-         int codigo=0;
+    private void eliminar(String nom){
         try {
             Connection conex = Conexion.getConexion ();
             Statement sta = conex.createStatement();
-            ResultSet rs = sta.executeQuery("SELECT id from Inscritos where Nombres_y_Apellidos = '"+ nom+"'");
-                codigo = rs.getInt("id");
-            }catch (SQLException ex){  
-            }
-         return codigo;
-    }
-    
-    public void eliminar(String nom){
-        try {
-            Connection conex = Conexion.getConexion();
-            Statement sta = conex.createStatement();
-            ResultSet rs = sta.executeQuery("delete from Participantes where nomParticipante = '"+nom+"'");
+            ResultSet rs = sta.executeQuery("delete from Participantes where nomParticipante ="+"'"+nom+"'" );
             }catch (SQLException ex){  
         }
     }
 }
-
